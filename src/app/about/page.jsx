460cf1e7299858e4714128a33b5e3189c6f7eb30
shahd al haj hasan <shahd345hasan@gmail.com>
2025-12-07 19:49:56 +0300

    home page and framer motion finished

---
+++ src/app/about/page.jsx
@@ -0,0 +1,16 @@
+"use client";
+import { motion } from "framer-motion";
+const AboutPage = () => {
+  return (
+    <motion.div
+      className="h-full"
+      initial={{ y: "-200vh" }}
+      animate={{ y: "0%" }}
+      transition={{ duration: 1 }}
+    >
+      About
+    </motion.div>
+  );
+};
+
+export default AboutPage;
---
+++ src/app/testanimation/page.jsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+const TestPage = () => {
+  return (
+    <div className="h-full flex items-center justify-center">
+      <motion.div
+        className="w-96 h-96 bg-red-400 rounded"
+        initial={{ x: -100 }}
+        animate={{ x: 400, y: 300, opacity: 0.5 }}
+        transition={{ delay: 2, duration: 4 }}
+      ></motion.div>
+    </div>
+  );
+};
+
+export default TestPage;
