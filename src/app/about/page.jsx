8ec329d8ed73f0ea8699a83885da24d30e2e0bef
shahd al haj hasan <shahd345hasan@gmail.com>
2025-12-10 08:17:35 +0300

    profile and contact page with framer motion finished

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -6,7 +6,6 @@ const AboutPage = () => {
   const containerRef = useRef();
   const { scrollYProgress } = useScroll({ container: containerRef });
   const skillRef = useRef();
-  // const isSkillRefInView = useInView(skillRef, { once: true });
   const isSkillRefInView = useInView(skillRef, { margin: "-100px" });
   const experienceRef = useRef();
   const isExperienceRefInView = useInView(experienceRef, { margin: "-100px" });
